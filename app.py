bfbe8a8ec3bd4925f49dba18519ba1c0fbe0c1b0
laurasfelix <laurasfelix@hotmail.com.br>
2023-07-16 02:37:35 -0500

    adding stuff

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-from Flask import flask
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import session
+from pymongo import MongoClient
+
+# -- Initialization section --
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
